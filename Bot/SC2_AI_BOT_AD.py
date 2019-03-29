8fd4bcca3a633866a6f571978205f0e6dbe09a35
DStrik <dannimb@gmail.com>
2019-03-29 14:59:30 +0000

    offensive units attack

--- Bot/SC2_AI_BOT_AD.py
+++ Bot/SC2_AI_BOT_AD.py
@@ -3,7 +3,7 @@ from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
 from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR, GATEWAY, \
     CYBERNETICSCORE, STALKER
-
+import random
 
 class ADBot(sc2.BotAI):
     async def on_step(self, iteration):
@@ -15,6 +15,7 @@ class ADBot(sc2.BotAI):
         await self.build_assimilator()  # getting gas
         await self.offensive_force_buildings()
         await self.build_offensive_force()
+        await self.attack()
 
     async def build_workers(self):
         # nexus = command center
@@ -35,15 +36,15 @@ class ADBot(sc2.BotAI):
 
     async def build_assimilator(self):
         for nexus in self.units(NEXUS).ready:
-            vaspenes = self.state.vespene_geyser.closer_than(15.0, nexus)
-            for vaspene in vaspenes:
+            vespenes = self.state.vespene_geyser.closer_than(15.0, nexus)
+            for vespene in vespenes:
                 if not self.can_afford(ASSIMILATOR):
                     break
-                worker = self.select_build_worker(vaspene.position)
+                worker = self.select_build_worker(vespene.position)
                 if worker is None:
                     break
-                if not self.units(ASSIMILATOR).closer_than(1.0, vaspene).exists:
-                    await self.do(worker.build(ASSIMILATOR, vaspene))
+                if not self.units(ASSIMILATOR).closer_than(1.0, vespene).exists:
+                    await self.do(worker.build(ASSIMILATOR, vespene))
     
     async def offensive_force_buildings(self):
         if self.units(PYLON).ready.exists:
@@ -60,6 +61,24 @@ class ADBot(sc2.BotAI):
         for gw in self.units(GATEWAY).ready.noqueue:
             if self.can_afford(STALKER) and self.supply_left > 0:
                 await self.do(gw.train(STALKER))
+
+    def find_target(self, state):
+        if len(self.known_enemy_units) > 0:
+            return random.choice(self.known_enemy_units)
+        elif len(self.known_enemy_structures) > 0:
+            return random.choice(self.known_enemy_structures)
+        else:
+            return self.enemy_start_locations[0]
+
+    async def attack(self):
+        if self.units(STALKER).amount > 15:
+            for s in self.units(STALKER).idle:
+                await self.do(s.attack(self.find_target(self.state)))
+
+        elif self.units(STALKER).amount > 3:
+            if len(self.known_enemy_units) > 0:
+                for s in self.units(STALKER).idle:
+                    await self.do(s.attack(random.choice(self.known_enemy_units)))
     
 run_game(maps.get("AbyssalReefLE"), [
     Bot(Race.Protoss, ADBot()),
