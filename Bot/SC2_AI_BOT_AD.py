adf51f8572fe061ea8e310ead4b04e2c7b57b8e2
ViktorSveins <viktorsveins@gmail.com>
2019-03-29 12:57:27 +0000

    changes classname

--- Bot/SC2_AI_BOT_AD.py
+++ Bot/SC2_AI_BOT_AD.py
@@ -3,13 +3,13 @@ from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
 
 
-class SentdeBot(sc2.BotAI):
+class ADBot(sc2.BotAI):
     async def on_step(self, iteration):
         # what to do every step
         await self.distribute_workers()  # in sc2/bot_ai.py
 
 
 run_game(maps.get("AbyssalReefLE"), [
-    Bot(Race.Protoss, SentdeBot()),
+    Bot(Race.Protoss, ADBot()),
     Computer(Race.Terran, Difficulty.Easy)
 ], realtime=True)
